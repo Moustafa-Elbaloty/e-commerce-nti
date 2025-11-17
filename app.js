f3ac957aa76539c4a951e36562b1b408fbebb85f
Mohamed Soliman <157769377+msm444@users.noreply.github.com>
2025-11-17 15:29:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ app.use(express.json());
 //  الاتصال بقاعدة البيانات
 connectDB();
 
+// ✅ مسارات المستخدم
 //  استدعاء راوت المستخدمين
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
+
+const vendorRoutes = require("./routes/vendorRoutes");
+app.use("/api/vendor", vendorRoutes);
+
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
